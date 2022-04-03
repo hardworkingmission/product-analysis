e4bd2e15f8fe6b2e403aad0566d96c6a1f6b5303
hardworkingmission <shakil20131101041@gmail.com>
2022-04-04 02:43:28 +0600

    chart-added

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <footer className=' mx-auto py-5 flex items-center justify-center bg-pink-600 stiky bottom-0 w-5/6'>
+        <footer className=' mx-auto py-5 flex items-center justify-center bg-pink-600 stiky bottom-0 md:w-5/6'>
             <h1 className='text-xl font-bold text-white'>Copyright &copy; honey-center.com</h1>
             
         </footer>
