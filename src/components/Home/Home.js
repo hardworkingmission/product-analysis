b99c10e33eb7d35e69d8194e04f484ba0dab87a9
hardworkingmission <shakil20131101041@gmail.com>
2022-04-04 13:42:25 +0600

    content-updated

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -23,7 +23,7 @@ const Home = () => {
                 <div className=" lg:w-6/12 md:w-6/12 sm:w-full w-full p-3 flex items-center justify-center">
                     <div>
                         <h1 className='text-4xl text-pink-600 font-bold'>Honey Center</h1>
-                        <p className='my-3 text-lg'>We are the Leading pure Honey provider in Bangladesh.We have Big Consumer Network which is built by Our Reputation.We did not compromise the quality of our products from establishment.So you keep trust on us..</p>
+                        <p className='my-3 text-lg'>We are the Leading pure Honey provider in the Uk.We have Big Consumer Network which is built by Our Reputation.We did not compromise the quality of our products from establishment.So you keep trust on us..</p>
                         <button className='px-2 py-1 bg-pink-600 hover:bg-pink-800 text-lg text-white rounded-lg'>Live Store</button>
                     </div>  
                 </div>
